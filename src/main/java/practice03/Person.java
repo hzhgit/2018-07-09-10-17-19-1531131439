e27b78f9394bd81fc61deacc74895fe172f81e37
ZACH HE (ITA-ISDC-ISD-OOCLL/ZHA) <HEZA2@OOCL.COM>
2020-07-22 15:55:41 +0800

    [03][zach][feature] finish practice 03

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,31 @@
 package practice03;
 
 public class Person {
+    public String name;
+    public int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return ("My name is " + this.getName() + ". I am " + this.getAge() +" years old.");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,23 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    protected int Klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        return ("I am a Student. I am at Class " + this.getKlass() + ".");
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
 }
