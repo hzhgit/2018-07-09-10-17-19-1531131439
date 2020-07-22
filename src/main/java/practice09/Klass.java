6290ccc9eff1067cd3bc52d41ad7b9ef07573ac4
zach-he <971499086@qq.com>
2020-07-22 21:30:12 +0800

    [09][zach][feature] finish practice 09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,41 @@
 package practice09;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private ArrayList<Student> members = new ArrayList<Student>();
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student leader) {
+        if (members.contains(leader)) {
+            this.leader = leader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        members.add(student);
+    }
 }
