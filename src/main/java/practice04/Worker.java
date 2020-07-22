98c5f9c7dc697838749577adbcbea899112daad4
ZACH HE (ITA-ISDC-ISD-OOCLL/ZHA) <HEZA2@OOCL.COM>
2020-07-22 16:04:18 +0800

    [04][zach][feature] finish practice 04

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,13 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce(){
+        return ("My name is Tom. I am "+ this.getAge() +" years old. I am a Worker. I have a job.");
+    }
 }
