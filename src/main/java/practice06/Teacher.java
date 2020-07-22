0900889f2917e8eda676ada617a406e4977ce01a
ZACH HE (ITA-ISDC-ISD-OOCLL/ZHA) <HEZA2@OOCL.COM>
2020-07-22 16:21:33 +0800

    [06][zach][feature] finish practice 06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    protected int Klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    public Teacher(String name, int age){
+        super(name, age);
+    }
+
+    @Override
+    public String introduce(){
+        String Klass = "";
+        if (this.getKlass() > 0){
+            return ("My name is Tom. I am "+ this.getAge() + " years old. I am a Teacher. I teach Class "+ this.getKlass()+".");
+        }else{
+            return ("My name is Tom. I am "+ this.getAge() + " years old. I am a Teacher. I teach No Class.");
+        }
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
 }
